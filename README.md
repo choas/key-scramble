d8112014e0545f35886d72d97dea3d47b055c548
Lars Gregori <lars.gregori@sap.com>
2018-10-31 10:34:51 +0100

    add resetNumber parameter

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ In case you want to document something which contains e.g. a client id and clien
 ## Command Line
 
 ```sh
-./bin/key-scramble key
+./bin/key-scramble key [--resetNumber]
 ```
+
+With the resetNumber parameter all numbers will be changed to '9' and other characters aren't touched.
 
 In case your key contains one ore more ! characters, bash will show an error message __event not found__. Turn off history with following command:
 
--- key-scramble.js
+++ key-scramble.js
@@ -6,20 +6,27 @@ function newchar(base, max) {
   return String.fromCharCode(base.charCodeAt(0) + Math.floor(Math.random() * max))
 }
 
-function scramble(key) {
+function scramble(key, resetNumber) {
 
   var newkey = "";
   String(key).split("").forEach((c) => {
-    if (c >= "A" && c <= "Z") {
-      newkey += newchar("A", 26);
-    } else if (c >= "a" && c <= "z") {
-      newkey += newchar("a", 26);
-    } else if (c >= "0" && c <= "9") {
-      newkey += newchar("0", 10);
+    if (resetNumber && resetNumber === "--resetNumber") {
+      if (c >= "0" && c <= "9") {
+        newkey += "9";
+      } else {
+        newkey += c;
+      }
     } else {
-      newkey += c;
+      if (c >= "A" && c <= "Z") {
+        newkey += newchar("A", 26);
+      } else if (c >= "a" && c <= "z") {
+        newkey += newchar("a", 26);
+      } else if (c >= "0" && c <= "9" && !resetNumber) {
+        newkey += newchar("0", 10);
+      } else {
+        newkey += c;
+      }
     }
-
   })
   return newkey;
 }
